7d09db07fe5f558ab566350019a580d12a1809cc
Admin <Admin@D20833>
2026-05-14 09:41:17 +0700

    Add project files.

---
+++ CMS.Data/Entities/CategoryProduct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+/*
+ * Sinh vien :Mguyen Xuan Truong
+ * Ma so:2123110005
+ * Version 01
+ * 
+ */
+
+
+
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class CategoryProduct
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên danh mục không được để trống")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều sản phẩm
+        public virtual ICollection<Product>? Products { get; set; }
+    }
+
+}
---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Sinh vien :Mguyen Xuan Truong
+ * Ma so:2123110005
+ * Version 01
+ * 
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+
+}
